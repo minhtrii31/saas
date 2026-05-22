2b1f3deab453cacab2cdf111b7052fc3e953ab78
minhtrii31 <minhtri3101200@gmail.com>
2026-05-22 13:31:00 +0700

    Add frontend smoke testing

---
+++ apps/web/tests/e2e/home.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "@playwright/test";
+
+test("home page renders", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(
+    page.getByRole("heading", { name: /to get started, edit the page\.tsx file/i }),
+  ).toBeVisible();
+});
